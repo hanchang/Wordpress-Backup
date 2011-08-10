a522e2d50d030dfb02f4c341488b200b7469a456
Han <chang@thenetcircle.com>
2011-08-10 15:04:24 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordpressbackup::Application.config.secret_token = '1d136a58558da83e6f6af9599e67f5c89aee6f259440437e3ed0663fbe444077ffd53261fe33d34ad10d0c845ce7c57dc718f8a344fab16fa9edd29490efc61e'
